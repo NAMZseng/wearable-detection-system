e86dc3380e74623752831eed3bc5656230d58d54
NAMZseng <nanrong_z@foxmail.com>
2021-10-16 14:59:51 +0800

    完成血压预测api

--- wearable_detection_backend/deep_learning/views.py
+++ wearable_detection_backend/deep_learning/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import numpy as np
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from main import bp_predict
+
+
+@api_view(['POST'])
+def get_bp_predict(request):
+    """
+    对安卓的传入的5s内ecg,ppg的信号数据进行神经网络模型预测
+    :param request: post请求的json数据
+    :return: 5s内的平均收缩压SBP,舒张压DBP
+    """
+    if request.method == 'POST':
+        ecg = request.data.get('ecg')  # list
+        ppg = request.data.get('ppg')
+
+        # list to array in shape (1, 625)
+        ecg = np.array(ecg).reshape(1, 625)
+        ppg = np.array(ppg).reshape(1, 625)
+
+        # (1, 625) to (2, 625)
+        net_input_data = np.append(ecg, ppg, axis=0)
+
+        predict_result = bp_predict(net_input_data)
+
+        return Response(predict_result, status=status.HTTP_200_OK)
--- wearable_detection_backend/wearable_detection_backend/settings.py
+++ wearable_detection_backend/wearable_detection_backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,16 +26,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'deep_learning.apps.DeepLearningConfig',
 ]
 
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wearable_detection_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,20 +99,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
+USE_TZ = False
 USE_I18N = True
+USE_L10N = False
 
-USE_L10N = True
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
 
-USE_TZ = True
-
+SIMPLEUI_HOME_INFO = False
+SIMPLEUI_ANALYSIS = False
+DATETIME_FORMAT = 'Y-m-d H:i:s'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
